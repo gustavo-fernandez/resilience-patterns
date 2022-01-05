7a9e2c292315ef84ecb01b62eaa35e83cf92ae58
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2022-01-04 20:49:09 -0500

    rate limiter, bulk head, retry and time limiter

--- src/main/java/com/example/resiliencepatterns/CustomerController.java
+++ src/main/java/com/example/resiliencepatterns/CustomerController.java
@@ -1,6 +1,11 @@
 package com.example.resiliencepatterns;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +30,34 @@ public class CustomerController {
   @GetMapping("api/employee")
   public Mono<Customer> findEmployee() {
     return Mono.just(new Customer(9L, "Employee"));
+  }
+
+  @TimeLimiter(name = "time-limiter-01")
+  @GetMapping("api/time-limiter-test")
+  public Mono<Customer> timeLimiterTest() {
+    return Mono.just(new Customer(9L, "Employee"))
+      .delayElement(Duration.ofSeconds(4));
+  }
+
+  @Retry(name = "retry-01")
+  @GetMapping("api/retry-test")
+  public Mono<Customer> retryTest() {
+    return Mono.<Customer>error(new RuntimeException("Error en \"retry\""))
+      .doOnSubscribe(x -> log.info("Reintentando"));
+  }
+
+  @RateLimiter(name = "rate-limiter-01")
+  @GetMapping("api/rate-limiter")
+  public Mono<Customer> rateLimiterTest() {
+    return Mono.just(new Customer(9L, "Employee"))
+      .delayElement(Duration.ofSeconds(2));
+  }
+
+  @Bulkhead(name = "bulkhead-01")
+  @GetMapping("api/bulkhead-01")
+  public Mono<Customer> bulkHead01() {
+    return Mono.just(new Customer(9L, "Employee"))
+      .delayElement(Duration.ofSeconds(2));
   }
 
 }
